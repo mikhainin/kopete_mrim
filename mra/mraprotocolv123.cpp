61fb711cfba8c1fb7ef36382fbd7dd4023e8dcc0
Mikhail Galanin <galanin@corp.mail.ru>
2012-12-06 11:21:46 +0400

    Chat text message...

--- mra/mraprotocolv123.cpp
+++ mra/mraprotocolv123.cpp
@@ -200,10 +200,13 @@ bool MRAProtocolV123::isChatTextMessage(int chatMessageType) {
             (chatMessageType == 0x41) ||
             (chatMessageType == 0x42) ||
             (chatMessageType == 0x43) ||
+            (chatMessageType == 0x45) ||
+            (chatMessageType == 0x47) ||
             (chatMessageType == 0x50) ||
             (chatMessageType == 0x54) ||
             (chatMessageType == 0x56) ||
-            (chatMessageType == 0x57)   ;
+            (chatMessageType == 0x57) 
+            ;
 }
 
 void MRAProtocolV123::receiveChatMembersList(MRAData & data, const QString &from) {
